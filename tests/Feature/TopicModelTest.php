6e2bb3fbbd715b562b9f7160d867fdd8ca091c7d
kinoshita0623 <kuutarousii@gmail.com>
2021-01-06 15:28:31 +0900

    add factoryを作成した

---
+++ tests/Feature/TopicModelTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TopicModelTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    use RefreshDatabase;
+    
+    public function testデータを作成することはできるか()
+    {
+
+    }
+
+    
+}
