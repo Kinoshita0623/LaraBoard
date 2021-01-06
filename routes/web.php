19529a55823e0235b8658fd2c664457981ff7093
yumekiti <yumekiti120440@gmail.com>
2021-01-06 16:26:37 +0900

    add コントローラー & change ルート

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TopicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//作成ページを返す
+Route::get('/create', [TopicController::class, 'new']);
