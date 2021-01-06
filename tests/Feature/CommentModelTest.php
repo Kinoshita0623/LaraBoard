e953518d71bd9d99f5991467354bfb125d8c6fed
kinoshita0623 <kuutarousii@gmail.com>
2021-01-06 22:33:01 +0900

    change テストコードを書いた

--- tests/Feature/CommentModelTest.php
+++ tests/Feature/CommentModelTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Topic;
 use App\Models\Comment;
+use PHPUnit\Framework\Assert;
 
 class CommentModelTest extends TestCase
 {
@@ -14,11 +15,17 @@ class CommentModelTest extends TestCase
 
     public function testCommentをTopicに作成することができるか()
     {
-
+        $topic = Topic::factory()->create();
+        $topic->comments()->save(Comment::factory()->make());
+        Assert::assertEquals(1, $topic->comments()->get()->count());
     }
 
     public function test作成されたCommentがTopicに関連づけられているか()
     {
+        $topic = Topic::factory()->create();
+        $comment = Comment::factory()->make();
+        $topic->comments()->save($comment);
+        Assert::assertEquals($comment->id, $topic->comments()->first()->id);
 
     }
 }
